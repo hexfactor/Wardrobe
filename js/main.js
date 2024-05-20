e5b7c250c8c73195c6057bcd11761977efc56586
theokkstar <70141751+hexfactor@users.noreply.github.com>
2024-05-21 00:13:00 +1000

    Dark mode and overall style improvements

--- js/main.js
+++ js/main.js
@@ -114,8 +114,24 @@ function displayItems() {
     items.forEach(function(item) {
         var div = document.createElement('div');
         div.id = 'gridItem';
-        div.innerHTML = '<img src="' + item.image + '"><h2>' + item.name + '</h2><p>' + item.color + ' ' + item.material + ' ' + item.piece + '</p><p>Wasing: ' + item.washTemperature + ' ' + item.washType + '</p><p>Drying: ' + item.dryTemperature + ' ' + item.dryType + '</p>'
-        div.innerHTML += '<div id="itemButtons"><button id="decrementItemUsesBtn" onclick="decrementItemUses(' + items.indexOf(item) + ')">−</button>' + item.uses + '<button id="incrementItemUsesBtn" onclick="incrementItemUses(' + items.indexOf(item) + ')">+</button><button id="resetItemUsesBtn" onclick="resetItemUses(' + items.indexOf(item) + ')">↺</button><button id="deleteItemBtn" onclick="deleteItem(' + items.indexOf(item) + ')">×</button></div>';
+        div.innerHTML = `
+            <img src="${item.image}">
+            <div id="itemDetails">
+                <h2>${item.name}</h2>
+                <p>${item.color} ${item.material} ${item.piece}</p>
+                <p>Washing: ${item.washTemperature} ${item.washType}</p>
+                <p>Drying: ${item.dryTemperature} ${item.dryType}</p>
+            </div>
+        `;
+        div.innerHTML += `
+            <div id="itemButtons">
+                <button id="decrementItemUsesBtn" onclick="decrementItemUses(${items.indexOf(item)})">−</button>
+                <button id="itemUseCount" onclick="resetItemUses(${items.indexOf(item)})">${item.uses}</button>
+                <button id="incrementItemUsesBtn" onclick="incrementItemUses(${items.indexOf(item)})">+</button>
+                <button id="editItemBtn" onclick="editItem(${items.indexOf(item)})">&#9998;</button>
+                <button id="deleteItemBtn" onclick="deleteItem(${items.indexOf(item)})">×</button>
+            </div>
+        `;
         grid.appendChild(div);
     });
 }
